57a7541559ef655abfd1fcbb0ecf8988cf52f46c
juanzea94 <zea15401@uvg.edu.gt>
2016-07-24 13:23:15 -0600

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,9 +1,11 @@
 
 /**
- 
+ *@author Juan Pablo Zea, Luis Najera
+ * @version 24.07.16
+ * 
  * @param <E>
  */
-public interface Stack<T> {
+public interface Stack<T> {                                                     //Interface del programa.
     void push(T e);
     T pop();
     T peek();
