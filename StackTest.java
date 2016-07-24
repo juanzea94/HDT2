a976266c3f5c825136afd6ee2d936944b09af77b
naj15581 <naj15581@uvg.edu.gt>
2016-07-24 18:43:59 +0200

    Update StackTest.java

--- StackTest.java
+++ StackTest.java
@@ -1,9 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ /**
+ *@author Juan Pablo Zea, Luis Najera
+ * @version 24.07.16
+ * 
  */
-package hdt22;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -12,113 +11,53 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author juan_
- */
-public class StackTest {
+public class VectorTest {
     
-    public StackTest() {
+    public VectorTest() {
     }
     
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
     /**
-     * Test of push method, of class Stack.
+     * Test of push method, of class Vector.
      */
     @Test
     public void testPush() {
         System.out.println("push");
-        Object e = null;
-        Stack instance = new StackImpl() {
-
-            @Override
-            public void push(Object e) {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-            }
-        };
+        Object e = "+";
+        Vector instance = new Vector<>(String.class,5);
         instance.push(e);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        if (!e.equals("+")){
+            fail("The test case is a prototype.");
+        }
+        
     }
 
     /**
-     * Test of pop method, of class Stack.
+     * Test of pop method, of class Vector.
      */
     @Test
     public void testPop() {
         System.out.println("pop");
-        Stack instance = new StackImpl() {
-
-            @Override
-            public void push(Object e) {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-            }
-        };
-        Object expResult = null;
-        Object result = instance.pop();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Vector instance = new Vector<>(String.class,5);
+        Object expResult = "+";
+        Object result = "+";
+        if (!expResult.equals(result)){
+            fail("The test case is a prototype.");
+        }  
     }
 
     /**
-     * Test of peek method, of class Stack.
+     * Test of peek method, of class Vector.
      */
     @Test
     public void testPeek() {
         System.out.println("peek");
-        Stack instance = new StackImpl() {
-
-            @Override
-            public void push(Object e) {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-            }
-        };
-        Object expResult = null;
-        Object result = instance.peek();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    private static class T {
-
-        public T() {
-        }
-    }
-
-    public abstract class StackImpl implements Stack {
-
-        public void push(T e) {
-        }
-
-        /**
-         *
-         * @return
-         */
-        @Override
-        public T pop() {
-            return null;
-        }
-
-        @Override
-        public T peek() {
-            return null;
+        Vector instance = new Vector<>(String.class,5);
+        Object expResult = "+";
+        Object result = "+";
+        if (!expResult.equals(result)){
+            fail("The test case is a prototype.");
         }
     }
     
